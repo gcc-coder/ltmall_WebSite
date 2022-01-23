07740a648e407d82398e6b8954b6a05151495d86
gcc-coder <firelong.guo@hotmail.com>
2022-01-23 17:17:34 +0800

    1.构建广告模型 2.创建商品goods应用，并构建相关模型 3.配置fastdfs文件存储，并导入图片数据至storage

--- ltmall/settings/dev.py
+++ ltmall/settings/dev.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'users',
     'oauth',
     'areas',
+    'contents',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ ltmall/utils/fastdfs/__init__.py
@@ -0,0 +1,12 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author：Rick
+@Blog：http://xxxxxx
+ 
+@File：__init__.py.py
+@Time：2022/1/23 16:25
+ 
+@Motto：不积跬步无以至千里，不积小流无以成江海！
+@Description：
+"""
