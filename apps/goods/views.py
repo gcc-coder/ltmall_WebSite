3a7452e69b6bc738a8dcf3ca75c21053adb69e67
gcc-coder <firelong.guo@hotmail.com>
2022-02-08 17:59:15 +0800

    实现面包屑导航

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from django.views import View
 from ltmall.utils.category import get_categories
+from ltmall.utils.breadcrumb import get_breadcrumb
+from django import http
+import logging
 
 
+logger = logging.getLogger('django')
 class GoodsListView(View):
     """商品列表页面"""
 
     def get(self, request, category_id, page_num):
-
+        # 查询商品类别
         categories = get_categories()
+
+        try:
+            # 查询面包屑
+            breadcrumb = get_breadcrumb(category_id)
+        except Exception as e:
+            logger.error(e)
+            return render(request, 'contents/404.html')
+
         context = {
-            'categories': categories
+            'categories': categories,
+            'breadcrumb': breadcrumb
         }
 
+        # 响应结果
         return render(request, "contents/list.html", context)
---
+++ ltmall/utils/breadcrumb.py
@@ -0,0 +1,44 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author：Rick
+@Blog：http://xxxxxx
+ 
+@File：breadcrumb.py
+@Time：2022/2/7 19:35
+ 
+@Motto：不积跬步无以至千里，不积小流无以成江海！
+@Description：
+"""
+from goods.models import GoodsCategory
+
+
+def get_breadcrumb(category_id):
+    # 面包屑导航  一级:一级  二级:一级+二级  三级:一级+二级+三级
+    """
+    breadcrumb = {'cat1':'', 'cat2': '', 'cat3': ''}
+    """
+    category = GoodsCategory.objects.get(id=category_id)
+    category3 = GoodsCategory.objects.filter(parent_id=category_id)
+
+    breadcrumb = {'cat1': '', 'cat2': '', 'cat3': ''}
+
+    if category.parent is None:
+        # 一级
+        breadcrumb['cat1'] = category
+    elif category3.count() == 0:
+        # 三级：若传入的category_id，在parent_id中查询不到，即为三级分类
+        cat2 = category.parent
+        breadcrumb = {
+            'cat1': cat2.parent,
+            'cat2': cat2,
+            'cat3': category
+        }
+    else:
+        # 二级
+        breadcrumb = {
+            'cat1': category.parent,
+            'cat2': category
+        }
+
+    return breadcrumb
